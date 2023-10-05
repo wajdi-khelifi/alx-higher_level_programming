946c0b189dd00d5ddd15dfa91e1a4fcaaa240cc1
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-10-05 22:26:21 +0000

    program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,20 +4,20 @@ import sys
 
 if __name__ == "__main__":
     a = int(sys.argv[1])
-    operator = (sys.argv[2])
+    operators = (sys.argv[2])
     b = int(sys.argv[3])
-    argv = ((sys.argv) - 1)
+    argv = (len(sys.argv) - 1)
     if argv != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if operator == "+":
+    if operators == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == "-":
+    elif operators == "-":
         print("{} - {} = {}".format(a, b , sub(a, b)))
-    elif operator == "*"
+    elif operators == "*"
         print("{} * {} = {}".format(a, b, mul(a, b)))
         exit(1)
-    elif operator == "/"
+    elif operators == "/"
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
