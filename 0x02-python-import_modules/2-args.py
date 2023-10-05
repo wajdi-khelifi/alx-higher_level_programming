97942198fef93cfc6efcfc50ea499110cec570c3
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-10-05 17:11:18 +0000

    program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
             print("Number of argument(s): 0.")
             print(".")
         elif num_args == 1:
-            print("Number of argument(s): 1.")
+            print("Number of argument: 1.")
             print("1:", sys.argv[1])
         else:
             print(f"Number of argument(s): {num_args}.")
