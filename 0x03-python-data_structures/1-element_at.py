bea34e565be93d33bc916f4d845f7c3b86e66404
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-10-07 16:49:01 +0000

    function that retrieves an element from a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx not in range(my_list):
+    elif idx >= len(my_list):
         return None
     else:
-        for i in my_list:
-            print("Element at index {:d} is {}".format(idx, i))
+        return my_list[idx]
