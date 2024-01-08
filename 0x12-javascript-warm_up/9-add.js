d96a9b2c7e500656abef3c0676d263824ef07dfa
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-08 17:26:42 +0000

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,10 +2,10 @@
 const a = process.argv[2];
 const b = process.argv[3];
 function Add(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return(NaN);
-  } else {
-    return (parseInt(a) + parseInt(b));
-  }
+if (isNaN(a) || isNaN(b)) {
+return(NaN);
+} else {
+return (parseInt(a) + parseInt(b));
+}
 }
 console.log(Add(a, b));
