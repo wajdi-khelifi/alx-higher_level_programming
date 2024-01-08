7ccc3ac4a2ffcb04462718f4ed767d1e56adc924
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-08 17:06:27 +0000

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 if (isNaN(arg)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
 	for (let i = 0; i < arg; i++) {
 	  console.log('X'.repeat(arg));
