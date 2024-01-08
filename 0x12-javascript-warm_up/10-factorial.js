fba06338fa28540e7ba4d8a926175d841ef2a337
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-08 21:24:00 +0000

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,9 +2,9 @@
 const arg = process.argv[2];
 function Factorial (arg) {
   if (isNaN(arg) === true || arg === 1) {
-	  return (1);
+    return (1);
   } else {
-	  return (arg * Factorial(arg - 1));
+    return (arg * Factorial(arg - 1));
   }
 }
 console.log(Factorial(parseInt(arg)));
