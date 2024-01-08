e225eff9d56ef69f9e728457c4737dcdf8eb5896
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-08 16:37:08 +0000

    i love c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const convertNumber = parseInt(process.argv[2]);
 if (isNaN(convertNumber) === true) {
-  console.log('Missing number of occurences')
+  console.log('Missing number of occurences');
 } else {
 	for (let index = 0; index < convertNumber; index++) {
 	  console.log('C is fun');
