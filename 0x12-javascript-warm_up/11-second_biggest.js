7df62a03e0860ee5a96fb7669d695b4ab9ad669e
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-08 22:03:38 +0000

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const arg = process.argv.slice[2];
+
+const arg = process.argv.slice(2);
 let secondMax = 0;
 
 if (arg.length > 1) {
-  const args = arg.map(Number);
-  args.sort((a, b) => b - a);
-  secendMax = args[1];
+  const integers = arg.map(Number);
+  integers.sort((a, b) => b - a);
+  secondMax = integers[1];
 }
+
 console.log(secondMax);
