4999b3140ae5b853911ccf93c4557e19284b4878
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-09 16:56:35 +0000

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
 class Rectangle {
-  contructor (w, h) {
-    if ((w > 0) && ( h > 0)) {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
-      let r = '';
+      let s = '';
       for (let j = 0; j < this.width; j++) {
         s += 'X';
       }
-      console.log(r);
+      console.log(s);
     }
   }
 }
+
 module.exports = Rectangle;
