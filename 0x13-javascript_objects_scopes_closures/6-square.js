60d1b2a4402bbb908d0b0a9c608dbf5b0a0f0140
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-09 17:32:04 +0000

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const SquarePrevieus = require('./5-square.js');
+
+class Square extends SquarePrevieus {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+module.exports = Square;
