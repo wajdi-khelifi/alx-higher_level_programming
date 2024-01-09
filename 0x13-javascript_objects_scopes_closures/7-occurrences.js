a52c59b8628b93c1666f6fb3f1c62c950c18579b
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-09 17:43:34 +0000

    Task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let nbOccurences = 0;
-    for (let i = 0; i < list.lenght; i++) {
-      if (searchElement === list[i]) {
-        nbOccurences++;
-      }
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
     }
-    return nbOccurences;
+  }
+  return nOccurrences;
 };
