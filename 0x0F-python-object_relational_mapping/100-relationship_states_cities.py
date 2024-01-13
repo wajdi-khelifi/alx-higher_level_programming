53952296559a7ad22839f74c402005cd20fb13ea
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-13 18:12:30 +0000

    List relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    A script that creates the State "California" with the City "San Francisco"
+    from the database hbtn_0e_100_usa
+    Username, password and dbname wil be passed as arguments to the script
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import Base, City
+
+
+if __name__ == "__main__":
+    """Main execution"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(City(name="San Francisco", state=State(name="California")))
+    session.commit()
