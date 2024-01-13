38ce08cceb97d981ead687ea877c22f1181f6107
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-13 16:04:52 +0000

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,10 +12,15 @@ if __name__ == "__main__":
             db=sys.argv[3],
             host='localhost',
             port='3306')
+
     cursor = db.cursor()
+    
     cursor.execute("SELECT FROM * WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    
     rows = cursor.fetchall()
+    
     for row in rows:
         print(row)
+    
     cursor.close()
     db.close()
