bce3108e75029c4e2d89f047d6c79c15038df9b3
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-13 15:47:34 +0000

    list states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+"""Script to list states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main':
+    """execution"""
     data_base = MySQLdb.connect(user=sys.argv[1],
                                 password=sys.argv[2],
                                 data_base=sys.argv[3],
                                 host='localhost',
                                 port=3306)
     cursor = data_base.cursor()
+
     cursor.excute("SETECT * FROM states ORDER BY id ASC")
+
     data = cursor.fetchall()
 
     for row in data:
         print(row)
+
     cursor.close()
     data_base.close()
