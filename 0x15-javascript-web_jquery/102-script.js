de375a05b3b421849782067cb0f65c1526a9befd
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-02-28 23:32:17 +0100

    all

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('#btn_translate').click(function () {
+    const languageCode = $('#language_code').val();
+    
+    $.get(`https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`, function (data) {
+      if (data && data.hello) {
+        $('#hello').text(data.hello);
+      } else {
+        $('#hello').text('Translation not found');
+      }
+    });
+  });
+});
---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,20 @@
+$(function () {
+  $('#btn_translate').click(translateHello);
+  $('#language_code').keypress(function (event) {
+    if (event.which === 13) {
+      translateHello();
+    }
+  });
+  
+  function translateHello () {
+    const languageCode = $('#language_code').val();
+    
+    $.get(`https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`, function (data) {
+      if (data && data.hello) {
+        $('#hello').text(data.hello);
+      } else {
+        $('#hello').text('Translation not found');
+      }
+    });
+  }
+});
